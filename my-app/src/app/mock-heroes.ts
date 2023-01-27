51e214ef44fdab64f28f321fa105fc6b15f88fbd
Besta Alfidi <alfidibesta@gmail.com>
2023-01-27 16:48:42 +0700

    update Add services
    tour of heroes

---
+++ my-app/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: "Hulk"},
+    { id: 2, name: "Spiderman" },
+    { id: 3, name: "Captain America" },
+    { id: 4, name: "Dr Strange" },
+    { id: 5, name: "Iron Man" },
+    { id: 6, name: "Ant-man" },
+    { id: 7, name: "Deadpool" },
+    { id: 8, name: "Wolferine"},
+    { id: 9, name: "Thor"},
+    { id: 10, name: "Loki" },
+    { id: 11, name: "Medusa" }
+];
